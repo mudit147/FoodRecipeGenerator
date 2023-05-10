91e63163dd6443c73606695b713ac8058633f9f7
Mudit Mittal <muditmittal@Mudits-MacBook-Air.local>
2023-05-10 19:22:11 -0400

    added routes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,8 +8,12 @@ import { Recipe } from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
   @Input() index:number;
 
-  ngOnInit(): void {}
+  constructor(private recipeService:RecipeService){}
+
+  ngOnInit(){
+    this.recipe = this.recipeService.getRecipe(this.index);
+  }
 }
